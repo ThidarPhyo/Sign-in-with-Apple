3651d8c34d902de971f01afbc6f887175a31357a
ThidarPhyo <thidarphyo@comquas.tech>
2020-05-23 01:52:38 +0630

    Initial Commit

---
+++ AppleLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleLogin
+//
+//  Created by Thidar Phyo on 5/23/20.
+//  Copyright © 2020 Thidar Phyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
